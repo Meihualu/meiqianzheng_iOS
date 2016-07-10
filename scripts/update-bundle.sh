fbec9edea1d1837bf618e95a28f0288222ed4375
msn <msn@nickdeMacBook-Pro.local>
2016-07-10 16:37:11 +0800

    modify update-bundle.sh file

--- scripts/update-bundle.sh
+++ scripts/update-bundle.sh
@@ -1,3 +1,4 @@
+echo "update-bundle.sh start"
 if [ ! -z "$INFO_PLIST" ]; then
   /usr/libexec/PlistBuddy -c "Set :CFBundleVersion $TRAVIS_BUILD_NUMBER" "$INFO_PLIST"
   echo "Set CFBundleVersion to $TRAVIS_BUILD_NUMBER"
@@ -12,3 +13,4 @@ if [ ! -z "$BUNDLE_DISPLAY_NAME" ]; then
   /usr/libexec/PlistBuddy -c "Set :CFBundleDisplayName $BUNDLE_DISPLAY_NAME" "$INFO_PLIST"
   echo "Set CFBundleDisplayName to $BUNDLE_DISPLAY_NAME"
 fi
+echo "update-bundle.sh end"
