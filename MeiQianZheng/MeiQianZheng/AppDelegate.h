78d726ce3811a9f80f5ecf629be962b4a9529051
msn <msn@nickdeMacBook-Pro.local>
2016-07-10 14:19:26 +0800

    first commit

---
+++ MeiQianZheng/MeiQianZheng/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MeiQianZheng
+//
+//  Created by msn on 16/7/10.
+//  Copyright © 2016年 ZYL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MeiQianZheng/MeiQianZheng/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MeiQianZheng
+//
+//  Created by msn on 16/7/10.
+//  Copyright © 2016年 ZYL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
